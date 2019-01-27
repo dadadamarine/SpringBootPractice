553e8c3cfca359bddeb76bf34ac1b05cfc9f6795
dadadamarine <dadadamarine@naver.com>
2019-01-28 01:01:26 +0900

    add : Data관리 1. Posts 클래스 추가. 2. 테스팅 소스 추가

---
+++ src/main/java/com/dadadamarine/webservice/domain/PostsRepositoryTest.java
@@ -0,0 +1,41 @@
+package com.dadadamarine.webservice.domain;
+
+import org.junit.After;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PostsRepositoryTest {
+
+    @Autowired
+    PostsRepository postsRepository;
+
+    @After
+    public void cleanup() {
+        postsRepository.deleteAll();
+    }
+
+    @Test
+    public void 게시글저장_불러오기() {
+        postsRepository.save(Posts.builder()
+                .title("테스트 제목")
+                .content("테스트 본문")
+                .author("dadadamarine@gmail.com")
+                .build());
+
+        List<Posts> postsList = postsRepository.findAll();
+
+        Posts posts = postsList.get(0);
+        assertThat(posts.getTitle(), is("테스트 제목"));
+        assertThat(posts.getContent(), is("테스트 본문"));
+    }
+}
--- src/test/java/com/dadadamarine/webservice/SpringWebserviceApplicationTests.java
+++ src/test/java/com/dadadamarine/webservice/SpringWebserviceApplicationTests.java
@@ -1,4 +1,4 @@
-package com.dadadamarine.springwebservice;
+package com.dadadamarine.webservice;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
