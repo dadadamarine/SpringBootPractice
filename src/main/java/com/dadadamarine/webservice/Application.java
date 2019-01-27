4a647daf7907409b77623aeb9f8cd836a30b92e9
dadadamarine <dadadamarine@naver.com>
2019-01-28 04:13:36 +0900

    add : 데이터 모델에 created, modified 시간 기록
    3. posts -> 시간 상속 , JPA Auditing 활성화

--- src/main/java/com/dadadamarine/webservice/Application.java
+++ src/main/java/com/dadadamarine/webservice/Application.java
@@ -2,12 +2,14 @@ package com.dadadamarine.webservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
-public class SpringWebserviceApplication {
+public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringWebserviceApplication.class, args);
+        SpringApplication.run(Application.class, args);
     }
 
 }
