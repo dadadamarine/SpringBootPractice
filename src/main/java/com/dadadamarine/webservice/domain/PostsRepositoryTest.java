0638c849c099cda2d67a0feb96bac04d76297da0
dadadamarine <dadadamarine@naver.com>
2019-01-28 01:15:19 +0900

    issue : 테스팅 에러 1. 빈 생성 에러

--- src/main/java/com/dadadamarine/webservice/domain/PostsRepositoryTest.java
+++ src/main/java/com/dadadamarine/webservice/domain/PostsRepositoryTest.java
@@ -5,15 +5,18 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.support.AnnotationConfigContextLoader;
 
 import java.util.List;
 
-import static org.hamcrest.core.Is.is;
+import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@ContextConfiguration(classes = PostsRepositoryTest.class, loader = AnnotationConfigContextLoader.class)
 public class PostsRepositoryTest {
 
     @Autowired
